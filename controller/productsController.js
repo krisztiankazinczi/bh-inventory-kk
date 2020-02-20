b0fcd88f4dc224c40e2041c63236e1a59e9a8194
Gergely Kovacs <gergely.kovacs02@gmail.com>
2020-02-20 18:11:17 +0100

    dynamic-db-readout

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,7 +1,24 @@
 const products = require('../model/products');
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('inventory.db')
+
+
 function product(req, res) {
-    res.render('products', {products: products});
+    db.serialize(function() {
+        db.all("SELECT rowid, * FROM products", function(err, results) {
+            if (err != null) {
+                // hibakezelés
+            }
+            console.log(results);
+          res.render('products', {
+              pageTitle: 'Termékek',
+              products: results
+          })
+            
+        });
+      });
+    
 }
 
 module.exports = product;
